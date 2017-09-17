b03f3732ca87f131220d04941616b439bd27bc0c
lyuyeda <lyuyeda@gmail.com>
2017-09-17 04:47:25 -1000

    fixes

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,26 +1,31 @@
 function sumFor(list) {
   let ret = 0;
-  for(let i = 0; i < list.length; i++){
+  for (let i = 0; i < list.length; i++) {
     ret += list[i];
   }
   return ret;
 }
 function sumWhile(list) {
   let ret = 0;
-  while(list.length > 0){
+  while (list.length > 0) {
     ret += list.pop();
   }
   return ret;
 }
-function sumRecursion(list,ret) {
-  if(list.length == 0){
+function sumRecursion(list, ret) {
+  if (list.length === 0) {
     return 0;
   }
-  let temp = list.pop();
+  const temp = list.pop();
   return temp + sumRecursion(list, ret);
 }
 function sumTheSimpleWay(list) {
-  return _.reduce(list,function (memo, item) {
+  return _.reduce(list, function (memo, item) {
     return memo + item;
-  },0);
+  }, 0);
 }
+
+console.log(sumFor([1, 2, 3]));
+console.log(sumWhile([1, 2, 3]));
+console.log(sumRecursion([1, 2, 3]));
+console.log(sumTheSimpleWay([1, 2, 3]));
